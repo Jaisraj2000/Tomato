73b5e92fba4c27d3d0a824e591756a640876a11b
Jaisraj2000 <117913378+Jaisraj2000@users.noreply.github.com>
2024-08-29 08:39:50 +0530

    1St changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
+      <Link to='/Tomato'><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</Link>
+        <Link to='/Tomato' onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")}className={menu=="menu"?"active":""}>menu</a>
         <a href='#app-download' onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>mobile-app</a>
         <a href='#footer' onClick={()=>setMenu("contact-us")}className={menu=="contact-us"?"active":""}>contact-us</a>
